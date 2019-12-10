fe2dbeff668df2674bc7da2472687a84278f3300
NicolasAsis <nico.asis15@gmail.com>
2019-12-10 14:45:44 -0800

    Second commit

---
+++ App.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import Main from './comps/Main';
+
+function App(){
+  return(
+    <View style={{width:'100%',height:'100%',display:'flex',justifyContent:'center',alignItems:'center'}}>
+        <Main />
+    </View>
+  );
+}
+export default App;
---
+++ comps/Main.js
@@ -0,0 +1,11 @@
+import React, {useState,useEffect} from 'react';
+import {View,Text,Animated} from 'react-native';
+import AnimatedTut from './AnimatedTut';
+
+function Main(){
+    return(
+        <AnimatedTut />
+    );
+}
+
+export default Main;
